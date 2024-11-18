f40a1fe52793c0315c70a0f83cc2682df6b4cb6f
LEVENT YANIK <levent581@tsomtal.com>
2024-11-18 15:57:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/uygulamabirimi4sirasizde2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.uygulamabirimi4sirasizde2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.emptyLongSet
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onayla(view: View) {
+        val yas=findViewById<EditText>(R.id.editTextNumber).text.toString().toInt()
+        val durum=findViewById<Switch>(R.id.switch1)
+        if (yas>=18 && durum.isChecked){
+            Toast.makeText(this,"Ehliyet Alabilirsiniz",Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(this,"Ehliyet Alamazsınız",Toast.LENGTH_LONG).show()
+        }
+    }
+}
